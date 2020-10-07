c11136750cb8041c250a6d30bf68e4b95333c9ff
Simran Barve <simranbarve@Simrans-MacBook-Air.local>
2020-10-07 14:14:05 +0400

    commented init, isempty, isfull and enqueue functions.

--- LinearQueue.py
+++ LinearQueue.py
@@ -1,37 +1,45 @@
 class Queue:
 
+    '''arg 'queue_size' is automatically set to 7 if no value is passed'''
     def __init__(self, queue_size = 7):
         self.queue_size = queue_size
         self.queue = [None] * queue_size
         self.rear = -1
         self.front = -1
 
-    def isempty(self):
+    '''checks if the stack is empty (used in enqueue and dequeue methods)'''
+    def isempty(self) -> typing.Union[bool]:
+        '''if the rear pointer and front pointer are bith minus 1 the queue
+        is empty'''
         if self.rear and self.front == -1:
             return True
         else:
             return False
 
-    def isfull(self):
+    def isfull(self) -> typing.Union[bool]:
+        '''if the rear pointer is the same as the queue size the queue is full'''
         if self.rear == self.queue_size:
             return True
         else:
             return False
 
-    def enqueue(self, value):
+    def enqueue(self, value) -> typing.Union[bool]:
+        '''if the queue is full you cannot enqueue anything'''
         if not self.isfull():
+            '''if the queue is empty you have to increase the rear and front
+             pointer so they are both zero'''
             if self.isempty():
                 self.rear += 1
                 self.front += 1
-                self.queue[self.rear] =  value
             else:
                 self.rear += 1
-                self.queue[self.rear] =  value
+            '''assign the new value to the new rear pointer value in the queue'''
+            self.queue[self.rear] =  value
             return True
-        else:
-            return False
+        return False
 
-    def dequeue(self):
+    def dequeue(self) -> typing.Union[bool, int]:
+        '''if the
         if not self.isfull() and not self.isempty():
             returnval = self.queue[self.rear]
             self.queue[self.rear] = None
@@ -39,8 +47,11 @@ class Queue:
             return returnval
         return False
 
-    def printing(self):
-        print(self.queue)
+    def printing(self) -> typing.Union[bool]:
+        if not self.isempty():
+            print(self.queue)
+            return True
+        return False
 
 Queue1 = Queue(6)
 Queue1.enqueue(3)
