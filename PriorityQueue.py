e6c756ab5e35ee81c2627f003d922e74b0ac9538
Simran Barve <simranbarve@Simrans-MacBook-Air.local>
2020-10-09 10:22:38 +0400

    created printing function (testing purposes) and isempty function

--- PriorityQueue.py
+++ PriorityQueue.py
@@ -2,6 +2,19 @@ class PriorityQueue():
 
     def __init__(self, queue_size = 7):
         self.queue_size = queue_size
-        self.queue = [[None]* queue_size]
+        self.queue = [[None]]* queue_size
+        self.pointer = -1
 
-        
+    def printing(self):
+        print(self.queue)
+        return True
+
+    def isempty(self):
+        if self.pointer == -1:
+            return True
+        return False
+
+
+
+Queue1 = PriorityQueue()
+Queue1.printing()
